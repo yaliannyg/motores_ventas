8e876dfc2914a4f559602740539d906bb3a911ea
Yalianny González <yaliannysg@gmail.com>
2020-12-14 16:43:33 -0400

    cofiguracion inicial de google login

--- src/configuration/endPoint.js
+++ src/configuration/endPoint.js
@@ -3,7 +3,7 @@
 //   : "https://motoresventas.herokuapp.com/app";
 
 
-const endpointBase = "http://192.168.0.108:8888/app"
+// const endpointBase = "http://192.168.0.108:8888/app"
 // const endpointBase = "http://localhost:8888/app"
-  
+  const endpointBase = "http://ec2-18-220-124-191.us-east-2.compute.amazonaws.com:8888/"
 export default endpointBase;
