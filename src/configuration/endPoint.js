9ddea697b47ed98e29c06449aed0b36c6ca9845e
himymlulu <33762867+himymlulu@users.noreply.github.com>
2021-01-09 07:56:06 -0400

    update

--- src/configuration/endPoint.js
+++ src/configuration/endPoint.js
@@ -1,9 +1,2 @@
-// const endpointBase = global.__DEV__
-//   ? "http://192.168.1.5:8888/app"
-//   : "https://motoresventas.herokuapp.com/app";
-
-
-// const endpointBase = "http://192.168.0.108:8888/app"
-// const endpointBase = "http://localhost:8888/app"
-  const endpointBase = "http://ec2-18-220-124-191.us-east-2.compute.amazonaws.com:8888/"
+const endpointBase = "http://192.168.0.108:8888/app"
 export default endpointBase;
