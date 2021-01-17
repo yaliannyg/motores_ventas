af7f57ea4632091ac3bc31341658ac9986a1af28
Yalianny González <yaliannysg@gmail.com>
2021-01-17 15:11:10 -0400

    test

--- src/configuration/endPoint.js
+++ src/configuration/endPoint.js
@@ -5,7 +5,8 @@
 
 // const endpointBase = "http://192.168.0.108:8888/app"
 // const endpointBase = "http://localhost:8888/app"
-  const endpointBase = "https://motoresventas.herokuapp.com/app"
+  const endpointBase = "http://192.168.0.105:8888/app"
+  // "https://motoresventas.herokuapp.com/app"
   // :"http://192.168.0.105:8888/app"
 
 
